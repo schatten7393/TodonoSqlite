fa7dca9e7940bd459ec2dd8f9d9efcfd4cde37ed
schatten7393 <schattenornacht@gmail.com>
2024-10-31 23:33:40 +0600

    done

---
+++ app/src/main/java/com/example/todonosqlite/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.todonosqlite;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Lookup the listview in activity layout
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+
+        setupListViewListener();
+    }
+
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(
+                (adapter, item, pos, id) -> {
+                    // Remove the item within array at position
+                    items.remove(pos);
+                    // Refresh the adapter
+                    itemsAdapter.notifyDataSetChanged();
+                    // Return true consumes the long click event (marks it handled)
+                    return true;
+                }
+        );
+    }
+
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+    }
+}
